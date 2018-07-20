58c114a587e5530a24cf228c9656dd46fbedefb6
Abba Yosua <email@abbayosua.com>
2018-07-20 14:16:46 +0700

    First commit

--- modified number/index.js
+++ modified number/index.js
@@ -1,16 +1,19 @@
-//Variable Declaration 
+//Variable Declaration - Level 0
 var numberA = 10;
 let numberB = 099;
 const numberC = 0.02;
 const five = 5;
 const negative = -1;
-let firstphrase = "Whats up";
-let secondphrase = "nigga!!";
 
-//Operation 
+//Operation - Level 1
 hello = numberA + numberB;
 hellomin = numberB - numberA;
 hellodivide = numberA / five;
 hellox = numberA * five;
 hellopercent = numberA % 200;
-say = firstphrase . secondphrase;
+negative_operation = five + negative;
+
+//Operation - Level 2
+let word1 = "Whatsaupp ";
+let word2 = "mate ";
+phrase = '${word1} and ${word2}';
