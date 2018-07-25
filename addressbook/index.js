9feb7d7bcbab17fced79be8bb647c953af6aa624
Abba Yosua <email@abbayosua.com>
2018-07-26 05:54:24 +0700

    Improvement on addressbook Module

--- addressbook/index.js
+++ addressbook/index.js
@@ -1,9 +1,24 @@
-function addAddress() {
-    let userSubmit = prompt("Enter People's Address", "kandang");
+/* let getAddress() => {
+        let userSubmit = prompt("Enter People's Address", "kandang");
 
-    let newline = document.createElement("DIV");
-    let theaddress = document.createTextNode(userSubmit);
-    newline.appendChild(theaddress);
-    document.getElementById("peopleAddress").appendChild(newline);
-    event.preventDefault();
+        let newline = document.createElement("DIV");
+        let theaddress = document.createTextNode(userSubmit);
+        newline.appendChild(theaddress);
+        document.getElementById("peopleAddress").appendChild(newline);
+        event.preventDefault();
+    } */
+
+//the comparison
+
+function addAddress() {
+
+
+  let address = $("#theInput").val();
+  $("#peopleAddress").append("<div id='list-'>" + address + "<button onClick='deleteAddress()'>X</button> </div>");
+  event.preventDefault();
+}
+
+function deleteAddress() {
+ let wantToDelete = $('.list-1').text();
+ alert(wantToDelete);
 }
