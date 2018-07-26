9d526f85327b40b6346da6bc9403181287a7515b
Abba Yosua <email@abbayosua.com>
2018-07-26 08:55:58 +0700

    Added Background and modified the CSS

--- emailvalidator/index.js
+++ emailvalidator/index.js
@@ -1,13 +1,22 @@
-
 function receiveSpam() {
     let theEmail = $('#userEmail').val();
     let theValidation = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z]{2,10})+$/;
     let meCheck = theValidation.test(theEmail)
 
-    if (meCheck == true) {
-        let thePunisher = $('#theInfo').text('We have sent 1000 Spam Mail to your account, Enjoy')
+    
+    /*if (meCheck == true) {
+        let thePunisher = $('#theInfo').text('We have sent 1000 Spam Mail to your account, Enjoy');
     } else {
-        alert("are you sure doesnt want to receive Spam for entire of your life? The email that you input is invalid")
+        alert("are you sure doesnt want to receive Spam for entire of your life? The email that you input is invalid");
+    }*/
+
+    switch (meCheck) {
+        case true:
+        let thePunisher = $('#theInfo').text('We have sent 1000 Spam Mail to your account, Enjoy');
+        break;
+        case false:
+        alert("are you sure doesnt want to receive Spam for entire of your life? The email that you input is invalid");
+        break;
     }
 
     //prevent default dude
