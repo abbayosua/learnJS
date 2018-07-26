e64653f8cf2c1dbdd6bb4fe4abc9daaf6426f67b
Abba Yosua <email@abbayosua.com>
2018-07-26 07:01:10 +0700

    New folder - new learning Material = EmailValidator (in the future will be a Spam Generator)

---
+++ emailvalidator/index.js
@@ -0,0 +1,15 @@
+
+function receiveSpam() {
+    let theEmail = $('#userEmail').val();
+    let theValidation = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z]{2,10})+$/;
+    let meCheck = theValidation.test(theEmail)
+
+    if (meCheck == true) {
+        let thePunisher = $('#theInfo').text('We have sent 1000 Spam Mail to your account, Enjoy')
+    } else {
+        alert("are you sure doesnt want to receive Spam for entire of your life?")
+    }
+
+    //prevent default dude
+    event.preventDefault();
+}
